c9d2482a43f4504d0863d10eac1af2c9d8bf7b1d
Ladislav Sladecek <ladislav.sladecek@gmail.com>
2021-01-09 19:33:41 +0100

    Photo validation.

--- legalage/certifier/certifier/bin/main.js
+++ legalage/certifier/certifier/bin/main.js
@@ -2,10 +2,13 @@
 "use strict";
 
 const args = require( "../src/args" );
+const photo = require( "../src/photo" );
 
 const main = () => {
     const para = args.parse();
     args.print(para);
+    const ph = photo.load(para.p);
+
 };
 
 main();
